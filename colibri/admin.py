ef5ade455c9a443195277ceffc73b37f23b1f8fc
trigger <triggeruseromar@gmail.com>
2021-08-18 18:44:48 -0500

    Creacion de superusuario, datos dinamicos en plantillas y estilos

--- colibri/admin.py
+++ colibri/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Clima,Region,Productor,Producto,Presentacion
 # Register your models here.
+admin.site.register(Clima)
+admin.site.register(Region)
+admin.site.register(Productor)
+admin.site.register(Producto)
+admin.site.register(Presentacion)
--- colibri/views.py
+++ colibri/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-
+from .models import Region
 # Create your views here.
 
 def home(request):
     return render(request,'home.html')
 
 def term_cliente(request):
-
-    return render(request,'term_cliente.html' )
+    #Llamar traer todas las regiones 
+    regiones=Region.objects.all()
+    return render(request,'term_cliente.html', {'regiones':regiones} )
